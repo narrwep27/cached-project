9d6452f7bb82ec030601c6a1979e8fd597400690
npark27 <93529617+narrwep27@users.noreply.github.com>
2022-02-07 20:55:12 -0500

    made migrations; added models to admin.py; pipenv installed libraries for auth

--- cached/admin.py
+++ cached/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Expense, Tag, Goal
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Expense)
+admin.site.register(Tag)
+admin.site.register(Goal)
