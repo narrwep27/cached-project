f9fdaf26c6fd9e6208b8e9cbf5313eb78cc02b2a
npark27 <93529617+narrwep27@users.noreply.github.com>
2022-02-08 18:51:23 -0500

    made migration for small model changes; added urls.py in cached app

--- cached/models.py
+++ cached/models.py
@@ -27,22 +27,22 @@ class Tag(models.Model):
         return self.name
 
 class Expense(models.Model):
-    cost = models.DecimalField(max_digits=14, decimal_places=2)
+    cost = models.DecimalField(max_digits=15, decimal_places=2)
     date = models.DateField(default=date.today)
-    note = models.CharField(max_length=300)
+    note = models.CharField(max_length=300, blank=True, null=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='expenses')
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='expenses')
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
     def __str__(self):
-        return self.id
+        return str(self.cost)
 
 class Goal(models.Model):
-    goal = models.DecimalField(max_digits=14, decimal_places=2)
-    description = models.CharField(max_length=300)
+    goal = models.DecimalField(max_digits=15, decimal_places=2)
+    description = models.CharField(max_length=300, blank=True, null=True)
     month = models.DateField()
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='goals')
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
     def __str__(self):
-        return self.id
+        return str(self.goal)
