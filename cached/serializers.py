32cc66b9d75fc64411c4a3c09962f6fd11ff1922
npark27 <93529617+narrwep27@users.noreply.github.com>
2022-02-08 20:43:36 -0500

    register user endpoint working to create new user

--- cached/serializers.py
+++ cached/serializers.py
@@ -21,3 +21,16 @@ class GoalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Goal
         fields = ('id', 'goal', 'description', 'month', 'user')
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password')
+        extra_kwargs = { 'password': {'write_only': True} }
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
