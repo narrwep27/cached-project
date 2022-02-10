ef6eb7f99c4f67833afc94430f1d0ad738338578
npark27 <93529617+narrwep27@users.noreply.github.com>
2022-02-10 09:06:19 -0500

    moved simplejwt paths into cached cached.urls

--- cached/urls.py
+++ cached/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 urlpatterns = [
     path('register', views.CreateCustomUser.as_view(), name='create_user'),
+    path('login', TokenObtainPairView.as_view(), name='token_login'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify', TokenVerifyView.as_view(), name='token_verify'),
     path('listusers', views.ListUsers.as_view(), name='read_user_list'),
     path('user/<int:pk>', views.RetrieveUser.as_view(), name='read_user'),
     path('tag/create', views.CreateTag.as_view(), name='create_tag'),
