77b2fa507192a3ba629dcfb6f3e538022872dba1
npark27 <93529617+narrwep27@users.noreply.github.com>
2022-03-10 20:21:54 -0500

    added input to filter expenses by date in analysis.vue

--- cached-vue-app/src/pages/Analysis.vue
+++ cached-vue-app/src/pages/Analysis.vue
@@ -1,20 +1,28 @@
 <template>
     <div class="page analysis-comp">
-        <h2>Expense Analysis</h2>
-        <column-chart 
-            :data="tagExpenses" 
-            class="column-chart"
-            prefix="$"
-            width="60%"
-        />
-        <br />
-        <pie-chart 
-            :data="tagExpenses" 
-            class="pie-chart"
-            legend="right"
-            prefix="$"
-            width="35%"
-        />
+        <h2>Monthly Spending</h2>
+        <h2>Expenses by Tag</h2>
+            <form class="analysis-tag-chart-form" v-on:change="filterExpenseByDate">
+                <label>Choose the Date:</label>
+                <input type="month" placeholder="month" v-model="chartDate" />
+                <button v-on:click.prevent="getTodaysDate">Back to Today</button>
+            </form>
+        <div class="analysis-column-pie-div">
+            <column-chart 
+                :data="tagExpenses" 
+                class="analysis-column-chart"
+                prefix="$"
+                width="60%"
+            />
+            <br />
+            <pie-chart 
+                :data="tagExpenses" 
+                class="analysis-pie-chart"
+                legend="right"
+                prefix="$"
+                width="35%"
+            />
+        </div>
     </div>
 </template>
 
@@ -24,11 +32,15 @@ import { VerifyToken } from '../services/CustomUser';
 export default {
     name: 'Analysis',
     data: () => ({
-        tagExpenses: []
+        tagExpenses: [],
+        chartDate: "",
+        expensesByDate: [],
     }),
     async beforeMount() {
         await this.checkToken();
         this.getTagExpenses();
+        this.getTodaysDate();
+        this.filterExpenseByDate();
     },
     methods: {
         getTagExpenses() {
@@ -43,6 +55,18 @@ export default {
                 dataObj[tag.name] = totalCost;
             });
             this.tagExpenses = dataObj;
+        },
+        getTodaysDate() {
+            let today = new Date();
+            let month = `${today.getMonth() + 1}`
+            if (month.length === 1) { month = `0${month}`}
+            this.chartDate = `${today.getFullYear()}-${month}`
+        },
+        filterExpenseByDate() {
+            this.expensesByDate = this.$store.state.expenses.filter((exp) => {
+                let dateString = exp.date.slice(0, 7)
+                return dateString === this.chartDate
+            })
         },
         async checkToken() {
             if (localStorage.getItem('accessToken')) {
@@ -68,13 +92,31 @@ export default {
 </script>
 
 <style scoped>
-    .column-chart {
-        margin: 1em auto;
+    .analysis-tag-chart-form {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 1em;
+        width: fit-content;
+        padding: 1em;
+        margin: 0 3em;
+    }
+    .analysis-tag-chart-form > input[type="month"] {
+        font-size: 18px;
+        margin: .5em;
+    }
+    .analysis-tag-chart-form > button {
+        height: 2.7em;
+    }
+    .analysis-column-pie-div {
+        display: flex;
+        justify-content: space-evenly;
+        margin: 2em;
+    }
+    .analysis-column-chart {
         background-color: white;
         border: 1px solid #c0d7ee;
         border-radius: 5px;
-    }
-    .pie-chart {
-        margin: 1em auto;
+        padding: 1em;
     }
 </style>
